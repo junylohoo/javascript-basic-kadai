15b72ff7a0dd55ba5cafd01fffbb39fa1c32ba0f
junylohoo <ohmyyou12@icloud.com>
2025-04-08 14:59:21 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+const today= new Date("2024-10-12");
+
+const year = today.getFullYear();
+const month = today.getMonth() + 1;
+const day = today.getDate();
+
+   
+
+console.log('年: ${year}, 月:${month}, 日: ${day}');
